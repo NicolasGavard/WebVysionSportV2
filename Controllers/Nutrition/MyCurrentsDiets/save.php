25d929fcf140fef685eb2a18c281b41eac29df15
Nicolas <nicolas.gavard@ekim.fr>
2022-05-24 11:48:00 +0200

    New

--- Controllers/Nutrition/MyCurrentsDiets/save.php
+++ Controllers/Nutrition/MyCurrentsDiets/save.php
@@ -1,7 +1,8 @@
 <?php
 include(__DIR__ . "/../../../DistriXInit/DistriXSvcControllerInit.php");
 // DATA
-include(__DIR__ . "/../../Data/DistriXFoodBrandData.php");
+include(__DIR__ . "/../../Data/DistriXNutritionCurrentDietData.php");
+include(__DIR__ . "/../../Data/DistriXNutritionCurrentDietUsersData.php");
 // Error
 include(__DIR__ . "/../../../GlobalData/ApplicationErrorData.php");
 // Layer
@@ -16,25 +17,35 @@ $error        = array();
 $output       = array();
 $outputok     = false;
 
-$distriXCodeTableBrandData = new DistriXFoodBrandData();
-$distriXCodeTableBrandData->setId($_POST['id']);
-$distriXCodeTableBrandData->setName($_POST['name']);
-$distriXCodeTableBrandData->setLinkToPicture('');
-if($_POST['base64Img'] != '') { $distriXCodeTableBrandData->setLinkToPicture($_POST['base64Img']);}
-$distriXCodeTableBrandData->setTimestamp($_POST['timestamp']);
-$distriXCodeTableBrandData->setStatus($_POST['statut']);
+
+$distriXNutritionCurrentDietData = new DistriXNutritionCurrentDietData();
+$distriXNutritionCurrentDietData->setId($_POST['id']);
+$distriXNutritionCurrentDietData->setIdUser($_POST['id']);
+$distriXNutritionCurrentDietData->setIdDietTemplace($_POST['id']);
+$distriXNutritionCurrentDietData->setDateStart($_POST['dateStart']);
+$distriXNutritionCurrentDietData->setStatus($_POST['statut']);
+$distriXNutritionCurrentDietData->setTimestamp($_POST['timestamp']);
+
+
+$distriXCodeTableDietData = new DistriXFoodDietData();
+$distriXCodeTableDietData->setId($_POST['id']);
+$distriXCodeTableDietData->setName($_POST['name']);
+$distriXCodeTableDietData->setLinkToPicture('');
+if($_POST['base64Img'] != '') { $distriXCodeTableDietData->setLinkToPicture($_POST['base64Img']);}
+$distriXCodeTableDietData->setTimestamp($_POST['timestamp']);
+$distriXCodeTableDietData->setStatus($_POST['statut']);
 
 $servicesCaller = new DistriXServicesCaller();
-$servicesCaller->setMethodName("SaveBrand");
-$servicesCaller->addParameter("data", $distriXCodeTableBrandData);
-$servicesCaller->setServiceName("DistriXServices/Food/Brand/DistriXFoodBrandSaveDataSvc.php");
+$servicesCaller->setMethodName("SaveDiet");
+$servicesCaller->addParameter("data", $distriXCodeTableDietData);
+$servicesCaller->setServiceName("DistriXServices/Food/Diet/DistriXFoodDietSaveDataSvc.php");
 list($outputok, $output, $errorData) = $servicesCaller->call(); //var_dump($output);
 
-if (DistriXLogger::isLoggerRunning(__DIR__ . "/../../DistriXLoggerSettings.php", "Security_Brand")) {
+if (DistriXLogger::isLoggerRunning(__DIR__ . "/../../DistriXLoggerSettings.php", "Security_Diet")) {
   $logInfoData = new DistriXLoggerInfoData();
   $logInfoData->setLogIpAddress($_SERVER['REMOTE_ADDR']);
-  $logInfoData->setLogApplication("DistriXBrandSaveDataSvc");
-  $logInfoData->setLogFunction("SaveBrand");
+  $logInfoData->setLogApplication("DistriXDietSaveDataSvc");
+  $logInfoData->setLogFunction("SaveDiet");
   $logInfoData->setLogData(print_r($output, true));
   DistriXLogger::log($logInfoData);
 }
