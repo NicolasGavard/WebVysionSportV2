3cb252171155569f8d866796ae93086ab18c0f8b
Nicolas <nicolas.gavard@ekim.fr>
2022-05-24 14:15:40 +0200

    Diet

--- DistriXServices/Nutrition/CurrentDiet/DistriXNutritionMyCurrentsDietsViewDataSvc.php
+++ DistriXServices/Nutrition/CurrentDiet/DistriXNutritionMyCurrentsDietsViewDataSvc.php
@@ -25,20 +25,35 @@ $databasefile = __DIR__ . "/../../../DistriXServices/Db/Infodb.php";
 
 $dbConnection = null;
 $errorData    = null;
+$distriXNutritionCurrentDietData = new DistriXNutritionCurrentDietData();
 
 $dbConnection = new DistriXPDOConnection($databasefile, DISTRIX_STY_KEY_AES);
 if (is_null($dbConnection->getError())) {
-  $data = $dataSvc->getParameter("data");
-  $dietStor = StyApplicationStor::read($data->getId(), $dbConnection);
-  $infoApplication    = DistriXSvcUtil::setData($dietStor, "DistriXStyApplicationData");
+  $data                             = $dataSvc->getParameter("data");
+  $dietStor                         = DietStor::read($data->getId(), $dbConnection);
+  $distriXNutritionCurrentDietData  = DistriXSvcUtil::setData($dietStor, "DistriXNutritionCurrentDietData");
+  
+  $dietStudentStorData = new DietStudentStorData();
+  $dietStudentStorData->setIdDiet($dietStor->getId());
+  list($dietStudentStor, $dietStudentStorInd) = DietStudentStor::findByIdDiet($dietStudentStorData, false, $dbConnection);
+  foreach ($dietStudentStor as $student) {
+    $infoUser                             = DistriXStyUser::viewUser($student->getIdUser());
+    $distriXNutritionCurrentDietUsersData = new DistriXNutritionCurrentDietUsersData();
+    $distriXNutritionCurrentDietUsersData->setIdUser($infoUser->getId());
+    $distriXNutritionCurrentDietUsersData->setNameUser($infoUser->getName());
+    $distriXNutritionCurrentDietUsersData->setFirstNameUser($infoUser->getFirstName());
+    $currentDietAssignedUsers[] = $distriXNutritionCurrentDietUsersData;
+  }
+  $distriXNutritionCurrentDietData->setAssignedUsers($currentDietAssignedUsers);
+
 } else {
   $errorData = ApplicationErrorData::noDatabaseConnection(1, 32);
 }
 if ($errorData != null) {
-  $errorData->setApplicationModuleFunctionalityCodeAndFilename("DistrixSty", "ListApplications", $dataSvc->getMethodName(), basename(__FILE__));
+  $errorData->setApplicationModuleFunctionalityCodeAndFilename("DistrixSty", "ViewMyCurrentDiet", $dataSvc->getMethodName(), basename(__FILE__));
   $dataSvc->addErrorToResponse($errorData);
 }
-$dataSvc->addToResponse("ViewApplication", $infoApplication);
+$dataSvc->addToResponse("ViewMyCurrentDiet", $distriXNutritionCurrentDietData);
 
 // Return response
 $dataSvc->endOfService();
