a6ed998bf55eb8a24f74c525ca9134d53df36bc9
Nicolas <nicolas.gavard@ekim.fr>
2022-05-24 08:59:18 +0200

    I18

---
+++ i18/EN/EN.php
@@ -0,0 +1,113 @@
+{ 
+  "hello":    "Hello", 
+  "welcome":  "Welcome to WebVysion Sport", 
+  
+  "menu_home":        "Home", 
+  
+  "menu_sport":       "Sport", 
+  "menu_sport_1":     "Sport", 
+  "menu_sport_2":     "Sport", 
+  "menu_sport_3":     "Sport", 
+  "menu_sport_4":     "Sport", 
+
+  "menu_nutrition":               "Nutrition", 
+  "menu_nutrition_myDiet":        "My current diets", 
+  "menu_nutrition_myTempletDiet": "My diet templates", 
+  "menu_nutrition_myRecipe":      "My recipes", 
+  "menu_nutrition_myIngredient":  "Food items", 
+  
+  "menu_bilan":       "Report", 
+  "menu_bilan_1":     "Report", 
+  "menu_bilan_2":     "Report", 
+  "menu_bilan_3":     "Report", 
+  "menu_bilan_4":     "Report", 
+  
+  "menu_eleve":       "Students", 
+  "menu_eleve_1":     "Students", 
+  "menu_eleve_2":     "Students", 
+  "menu_eleve_3":     "Students", 
+  "menu_eleve_4":     "Students", 
+
+  "menu_ressource":   "Resources", 
+  
+  "menu_formule":     "Formulas", 
+  
+  "menu_parametre":   "Parameters", 
+  
+  "menu_messagerie":  "Messaging", 
+  
+  
+  "menu_admin":     "Administration", 
+  
+  "menu_users":     "Users", 
+  
+  "menu_right":     "Rights", 
+  
+  "menu_codeTables":            "Code tables", 
+  "menu_codeTables_EcoScore":   "Eco-Score", 
+  "menu_codeTables_NovaScore":  "Nova-Score", 
+  "menu_codeTables_NutriScore": "Nutri-Score", 
+  "menu_codeTables_Label":      "Labels", 
+  "menu_codeTables_WeightType": "Contents", 
+  "menu_codeTables_language":   "Languages",  
+  
+  "menu_profil_myProfil":   "My profile", 
+  "menu_profil_messages":   "Messages", 
+  "menu_profil_activities": "Activities", 
+  "menu_profil_faq":        "FAQ", 
+  "menu_profil_logout":     "Disconnect", 
+  
+  "page_profil_language_title":             "My profile", 
+  "page_profil_language_login":             "Login", 
+  "page_profil_language_role":              "Role", 
+  "page_profil_language_name":              "Name", 
+  "page_profil_language_firstname":         "First name", 
+  "page_profil_language_email":             "Email", 
+  "page_profil_language_emailBackup":       "Backup email", 
+  "page_profil_language_phone":             "Phone", 
+  "page_profil_language_mobile":            "Portable", 
+  "page_profil_language_langue":            "Language", 
+  "page_profil_language_update_title":      "Save my profile", 
+  
+  "page_profil_language_title_2":           "Change my password",
+  "page_profil_language_old_pass":          "Old Password", 
+  "page_profil_language_new_pass":          "New Password", 
+  "page_profil_language_confirm_pass":      "Confirm Password", 
+  "page_profil_language_change_pass_title": "Change my password", 
+  
+  "page_codeTables_language_title":         "List of languages", 
+  "page_codeTables_language_picture":       "Picture", 
+  "page_codeTables_language_code":          "Code", 
+  "page_codeTables_language_name":          "Name", 
+  "page_codeTables_language_status":        "status", 
+  "page_codeTables_language_action":        "actions", 
+  "page_codeTables_language_add_title":     "Add a language", 
+  "page_codeTables_language_update_title":  "Save a language", 
+  "page_codeTables_language_delete_title":  "Delete a language", 
+  "page_codeTables_language_restore_title": "Restore a language", 
+
+
+  "go":      "I'm going !", 
+
+  "page_all_close":           "Close", 
+  "page_all_add":             "Add", 
+  "page_all_update":          "Save", 
+  "page_all_initPass":        "Initialize the password", 
+  "page_all_view":            "View", 
+  "page_all_delete":          "Delete", 
+  "page_all_restore":         "Restore", 
+  "page_all_change_picture":  "Change the picture", 
+
+  "confirm_delete"  : "Confirm the deletion to ",
+  "confirm_restore" : "Confirm restoration for ",
+
+  "errorData_ok":               "Successful processing !", 
+  "errorData_ok_txt":           "Your data has been successfully registered.", 
+  "errorData_ko":               "Processing error !", 
+  "errorData_txt":              "An internal error has occurred !!", 
+  "errorData_txt_code":         "The code is missing !!", 
+  "errorData_txt_description":  "The name is missing !!",
+  "errorData_txt_description":  "The desc is missing !!",
+  
+  "footer_signature": "WebVysion"
+}
---
+++ i18/FR/home.php
@@ -0,0 +1,117 @@
+<?php
+
+  $hello    =  "Bonjour ";
+  $welcome  =  "Bienvenue à nouveau sur WebVysion Sport";
+  
+  $menu_home    = "Accueil";
+  
+  $menu_sport   = "Sport";
+  $menu_sport_1 = "Sport";
+  $menu_sport_2 = "Sport";
+  $menu_sport_3 = "Sport";
+  $menu_sport_4 = "Sport";
+
+  $menu_nutrition               = "Nutrition";
+  $menu_nutrition_myDiet        = "Mes diètes en cours";
+  $menu_nutrition_myTempletDiet = "Mes templates de diètes";
+  $menu_nutrition_myRecipe      = "Mes recettes";
+  $menu_nutrition_myIngredient  = "Aliments";
+  
+  $menu_bilan   =   "Bilan";
+  $menu_bilan_1 = "Bilan";
+  $menu_bilan_2 = "Bilan";
+  $menu_bilan_3 = "Bilan";
+  $menu_bilan_4 = "Bilan";
+  
+  $menu_eleve   =   "Elèves";
+  $menu_eleve_1 = "Elèves";
+  $menu_eleve_2 = "Elèves";
+  $menu_eleve_3 = "Elèves";
+  $menu_eleve_4 = "Elèves";
+
+  $menu_ressource   = "Ressources";
+  $menu_ressource_1 = "Ressources";
+  $menu_ressource_2 = "Ressources";
+  $menu_ressource_3 = "Ressources";
+  $menu_ressource_4 = "Ressources";
+  
+  $menu_formule   = "Formules";
+  $menu_formule_1 = "Formules";
+  $menu_formule_2 = "Formules";
+  $menu_formule_3 = "Formules";
+  $menu_formule_4 = "Formules";
+  
+  $menu_parametre   = "Paramètres";
+  $menu_parametre_1 = "Paramètres";
+  $menu_parametre_2 = "Paramètres";
+  $menu_parametre_3 = "Paramètres";
+  $menu_parametre_4 = "Paramètres";
+  
+  $menu_messagerie    = "Messagerie";
+  $menu_messagerie_1  = "Messagerie";
+  $menu_messagerie_2  = "Messagerie";
+  $menu_messagerie_3  = "Messagerie";
+  $menu_messagerie_4  = "Messagerie";
+
+  $menu_admin                   = "Administration";
+  $menu_admin_users             = "Utilisateurs";
+  $menu_admin_users_list        = "Liste des utilisateurs";
+  $menu_admin_enterprises_list  = "Liste des entreprises";
+  $menu_admin_usersTypes_list   = "Liste des types d'utilisateurs";
+  
+  $menu_right                       = "Droits";
+  $menu_right_applications_list     = "Liste des applications";
+  $menu_right_modules_list          = "Liste des modules";
+  $menu_right_functionalities_list  = "Liste des fonctionnalités";
+  $menu_right_roles_list            = "Liste des rôles";
+  $menu_right_rights_list           = "Liste des droits";
+  
+  $menu_food                  = "Aliments";
+  $menu_food_food_list        = "Liste des aliments";
+  $menu_food_brand_list       = "Liste des marques";
+  $menu_food_ecoScore_list    = "Liste des eco-Score";
+  $menu_food_novaScore_list   = "Liste des nova-Score";
+  $menu_food_nutriScore_list  = "Liste des nutri-Score";
+  $menu_food_label_list       = "Liste des labels";
+  
+  $menu_codeTables                    = "Tables de code";
+  $menu_codeTables_weightType_list    = "Liste des contenances";
+  $menu_codeTables_food_category_list = "Liste des catégories d'aliments";
+  $menu_codeTables_nutritional_list   = "Liste des info. nutritionnelles";
+  $menu_codeTables_language_list      = "Liste des langues";
+
+  $menu_profil_myProfil   = "Mon profil";
+  $menu_profil_messages   = "Message(s)";
+  $menu_profil_activities = "Activité(s)";
+  $menu_profil_faq        = "FAQ";
+  $menu_profil_logout     = "Déconnexion";
+  
+  $page_all_close           = "Fermer";
+  $page_all_add             = "Ajouter";
+  $page_all_update          = "Modifier";
+  $page_all_initPass        = "Initialiser le mot de passe";
+  $page_all_view            = "Voir";
+  $page_all_delete          = "Supprimer";
+  $page_all_restore         = "Restaurer";
+  $page_all_yes             = "OUI";
+  $page_all_no              = "NON";
+  $page_all_change_picture  = "Changer l'image";
+
+  $confirm_delete  = "Confirmez la suppression pour ";
+  $confirm_restore = "Confirmez la restoration pour ";
+
+  $errorData_login_ko           = "Erreur de connexion, nom d'utilisateur ou mot de passe invalide !";
+  $errorData_ok                 = "Traitement réussi !";
+  $errorData_ok_txt             = "Vos données ont été enregistrées avec succès.";
+  $errorData_ko                 = "Erreur de traitement !";
+  $errorData_txt                = "Une erreur interne s'est produite !!";
+  $errorData_txt_code           = "Le code est manquant !!";
+  $errorData_txt_name           = "Le nom est manquant !!";
+  $errorData_txt_description    = "La description est manquante !!";
+  $errorData_txt_abbreviation   = "L'abbréviation est manquante !!";
+  $errorData_txt_weight         = "La taille est manquante !!";
+  $errorData_txt_assignedUsers  = "Aucun utilisateurs n'a été selectionné !!";
+  $errorData_txt_duration       = "La durée est manquante !!";
+  $errorData_txt_date_start     = "La date de début est manquante !!";
+  
+  $footer_signature = "WebVysion";
