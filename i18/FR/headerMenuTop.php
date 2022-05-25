d91cb1dd8e1b8987b19c5263f7e20e9ce97d4d42
Nicolas <nicolas.gavard@hotmail.fr>
2022-05-25 10:34:54 +0200

    Lang

---
+++ i18/FR/headerMenuTop.php
@@ -0,0 +1,36 @@
+<?php
+  $menu_profil_myProfil   = "Mon profil";
+  $menu_profil_messages   = "Message(s)";
+  $menu_profil_activities = "Activité(s)";
+  $menu_profil_faq        = "FAQ";
+  $menu_profil_logout     = "Déconnexion";
+  
+  $page_all_close           = "Fermer";
+  $page_all_add             = "Ajouter";
+  $page_all_update          = "Modifier";
+  $page_all_initPass        = "Initialiser le mot de passe";
+  $page_all_view            = "Voir";
+  $page_all_delete          = "Supprimer";
+  $page_all_restore         = "Restaurer";
+  $page_all_yes             = "OUI";
+  $page_all_no              = "NON";
+  $page_all_change_picture  = "Changer l'image";
+
+  $confirm_delete  = "Confirmez la suppression pour ";
+  $confirm_restore = "Confirmez la restoration pour ";
+
+  $errorData_login_ko           = "Erreur de connexion, nom d'utilisateur ou mot de passe invalide !";
+  $errorData_ok                 = "Traitement réussi !";
+  $errorData_ok_txt             = "Vos données ont été enregistrées avec succès.";
+  $errorData_ko                 = "Erreur de traitement !";
+  $errorData_txt                = "Une erreur interne s'est produite !!";
+  $errorData_txt_code           = "Le code est manquant !!";
+  $errorData_txt_name           = "Le nom est manquant !!";
+  $errorData_txt_description    = "La description est manquante !!";
+  $errorData_txt_abbreviation   = "L'abbréviation est manquante !!";
+  $errorData_txt_weight         = "La taille est manquante !!";
+  $errorData_txt_assignedUsers  = "Aucun utilisateurs n'a été selectionné !!";
+  $errorData_txt_duration       = "La durée est manquante !!";
+  $errorData_txt_date_start     = "La date de début est manquante !!";
+  
+  $footer_signature = "WebVysion";
