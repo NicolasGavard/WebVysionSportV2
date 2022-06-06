4872b77cf3e2a8160057489aecf8a2d82b5f3998
Yvan <yvan.excoffier@hotmail.com>
2022-06-06 15:34:43 +0200

    .

---
+++ Controllers/Init/ControllerInit.php
@@ -0,0 +1,28 @@
+<?php // Needed to encode in UTF8 ààéàé //
+include(__DIR__ . "/../../DistriXInit/DistriXSvcControllerInit.php");
+// Error
+include(__DIR__ . "/../../GlobalData/ApplicationErrorData.php");
+// Layer
+include(__DIR__ . "/../Layers/DistriXServicesCaller.php");
+// DistriX LOGGER
+include(__DIR__ . "/../../DistriXLogger/DistriXLogger.php");
+include(__DIR__ . "/../../DistriXLogger/data/DistriXLoggerInfoData.php");
+
+$resp         = [];
+$error        = [];
+$output       = [];
+$outputok     = false;
+
+function logController(string $element, string $application, string $function, mixed $data): ?DistriXSvcErrorData
+{
+  $returnValue = null;
+  if (DistriXLogger::isLoggerRunning(__DIR__ . "/../../DistriXLoggerSettings.php", $element)) {
+    $logInfoData = new DistriXLoggerInfoData();
+    $logInfoData->setLogIpAddress($_SERVER['REMOTE_ADDR']);
+    $logInfoData->setLogApplication($application);
+    $logInfoData->setLogFunction($function);
+    $logInfoData->setLogData(print_r($data, true));
+    $returnValue = DistriXLogger::log($logInfoData);
+  }
+  return $returnValue;
+}
