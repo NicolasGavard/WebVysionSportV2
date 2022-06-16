c77436c3d6496ed618ca88b1b400cad2cd42dadf
Nicolas <nicolas.gavard@hotmail.fr>
2022-06-16 09:22:27 +0200

    .

---
+++ i18/FR/Nutrition/nutritionMyCurrentsDietsMealList.php
@@ -0,0 +1,20 @@
+<?php
+  $page_title_prev              = "Mes diètes en cours";
+  $page_title                   = "Les repas de la diète";
+  $page_days                    = "Jours";
+  $page_meal_type               = "Repas";
+  $page_meal_foods              = "Composition";
+  $page_meal_calorie            = "Calorie";
+  $page_meal_proetin            = "Protéine";
+  $page_meal_glucide            = "Glucide";
+  $page_meal_lipid              = "Lipide";
+  $page_meal_quantity           = "Quantité";
+  $page_action                  = "Actions";
+  $page_add_title               = "Ajouter un repas";
+  $page_update_title            = "Modifier le repas";
+  $page_delete_title            = "Supprimer le repas";
+  $page_restore_title           = "Restaurer le repas";
+
+  $errorData_txt_template       = "Le modèle de la template de repas est manquant !!";
+  $errorData_txt_student        = "Aucun élèves assignés sur la repas !!";
+  $errorData_txt_dateBegin      = "La date de début de la repas est manquante !!";
